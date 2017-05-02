3e80906adc3d234a749f67e67b8b84bdfe655269
Raynos <raynos2@gmail.com>
2017-05-01 17:11:08 -0700

    example-gateway/idl: add nested structs example

---
+++ examples/example-gateway/idl/endpoints/nested_structs/nested_structs.thrift
@@ -0,0 +1,96 @@
+namespace java com.uber.zanzibar.endpoints.nestedStructs
+
+typedef string ItemType
+const ItemType T_OPTIONAL = "optional"
+const ItemType T_REQUIRED = "required"
+
+enum ItemState {
+	REQUIRED,
+	OPTIONAL
+}
+
+typedef string UUID
+
+struct RequiredExtra {
+	1: required list<string> one
+	2: required list<OptionalStruct> two
+	3: required set<string> three
+	4: required set<OptionalStruct> four
+	5: required map<string, string> five
+	6: required map<string, OptionalStruct> six
+	7: required UUID seven
+	8: required ItemType eight
+	9: required ItemState nine
+	10: required OptionalExtra ten
+}
+
+struct OptionalExtra {
+	1: optional list<string> one
+	2: optional list<OptionalStruct> two
+	3: optional set<string> three
+	4: optional set<OptionalStruct> four
+	5: optional map<string, string> five
+	6: optional map<string, OptionalStruct> six
+	7: optional UUID seven
+	8: optional ItemType eight
+	9: optional ItemState nine
+	10: optional RequiredExtra ten
+}
+
+struct RequiredStruct {
+	1: required bool one
+	2: required byte two
+	3: required i16 three
+	4: required i32 four
+	5: required i64 five
+	6: required double six
+	7: required binary seven
+	8: required string eight
+	9: required RequiredExtra nine
+}
+
+struct OptionalStruct {
+	1: optional bool one
+	2: optional byte two
+	3: optional i16 three
+	4: optional i32 four
+	5: optional i64 five
+	6: optional double six
+	7: optional binary seven
+	8: optional string eight
+	9: optional OptionalExtra nine
+}
+
+service NestedStructs {
+	RequiredStruct first(
+		1: required bool one
+		2: required byte two
+		3: required i16 three
+		4: required i32 four
+		5: required i64 five
+		6: required double six
+		7: required binary seven
+		8: required string eight
+		9: required RequiredStruct nine
+	) (
+		zanzibar.http.method = "POST"
+		zanzibar.http.path = "/first"
+		zanzibar.http.status = "200"
+	)
+	OptionalStruct second(
+		1: optional bool one
+		2: optional byte two
+		3: optional i16 three
+		4: optional i32 four
+		5: optional i64 five
+		6: optional double six
+		7: optional binary seven
+		8: optional string eight
+		9: optional OptionalStruct nine
+	) (
+		zanzibar.http.method = "POST"
+		zanzibar.http.path = "/second"
+		zanzibar.http.status = "200"
+	)
+}
+
